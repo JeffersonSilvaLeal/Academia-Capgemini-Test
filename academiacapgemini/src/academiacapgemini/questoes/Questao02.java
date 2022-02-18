e5934fb266af030fe46eee7a9f41a5417709a63f
JeffersonSilva <drankhar@gmail.com>
2022-02-18 15:00:20 -0300

    Commit Questão 03 Anagramas

--- academiacapgemini/src/academiacapgemini/questoes/Questao02.java
+++ academiacapgemini/src/academiacapgemini/questoes/Questao02.java
@@ -5,22 +5,18 @@ import java.util.Scanner;
 public class Questao02 {
 
 	public static void main(String[] args) {
-	
-	System.out.println("Digite sua senha: ");
-	System.out.println("DICA");
-	System.out.println("Deve Possui no mínimo 6 caracteres.\r\n"
-			+ "Deve Contér no mínimo 1 digito.\r\n"
-			+ "Deve Contér no mínimo 1 letra em minúsculo.\r\n"
-			+ "Deve Contér no mínimo 1 letra em maiúsculo.\r\n"
-			+ "Deve Contér no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+\r\n"
-			+ "");
-	// Entrada de dados
-	Scanner sc = new Scanner(System.in);
-	String password = sc.next();
-	
-	// Senha teste 'ab-cA@9'
-	Questao02.validaSenha(password);
-	
+
+		System.out.println("Digite sua senha: ");
+		System.out.println("DICA");
+		System.out.println("Deve Possui no mínimo 6 caracteres.\r\n" + "Deve Contér no mínimo 1 digito.\r\n"
+				+ "Deve Contér no mínimo 1 letra em minúsculo.\r\n" + "Deve Contér no mínimo 1 letra em maiúsculo.\r\n"
+				+ "Deve Contér no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+\r\n" + "");
+		// Entrada de dados
+		Scanner sc = new Scanner(System.in);
+		String password = sc.next();
+
+		// Senha teste 'ab-cA@6'
+		Questao02.validaSenha(password);
 	}
 
 	public static void validaSenha(String senha) {
@@ -28,25 +24,26 @@ public class Questao02 {
 		if (senha != null) {
 
 			if (senha.length() < 6) {
-				System.out.println("A SENHE DEVE CONTER NO MINIMO 6 CARACTERS.");
+				System.out.println("A SENHE DEVE CONTER NO MÃ�NIMO 6 CARACTERS.");
 
 			} else if (!Questao02.validaCaractersSenha(senha, 1)) {
-				System.out.println("A SENHE DEVE CONTER NO MINIMO 1 CARACTER MAIUSCULO.");
+				System.out.println("A SENHE DEVE CONTER NO MÃ�NIMO 1 CARACTER MAIUSCULO.");
 
 			} else if (!Questao02.validaCaractersSenha(senha, 2)) {
-				System.out.println("A SENHE DEVE CONTER NO MINIMO 1 CARACTER MINUSCULO.");
+				System.out.println("A SENHE DEVE CONTER NO MÃ�NIMO 1 CARACTER MINUSCULO.");
 
 			} else if (!Questao02.validaCaractersSenha(senha, 3)) {
-				System.out.println("A SENHE DEVE CONTER NO MINIMO 1 DIGITO.");
+				System.out.println("A SENHE DEVE CONTER NO MÃ�NIMO 1 DIGITO.");
 
 			} else if (!Questao02.validaCaractersSenha(senha, 4)) {
-				System.out.println("A SENHE DEVE CONTER NO MINIMO 1 CARACTER ESPECIAL EX. '!@#$%^&*()-+'.");
+				System.out.println("A SENHE DEVE CONTER NO MÃ�NIMO 1 CARACTER ESPECIAL EX. '!@#$%^&*()-+'.");
 
-			} else {
+
+			} else
 				System.out.println("A SENHA ATENDEU AOS REQUISITOS.");
 			}
 		}
-	}
+	
 
 	public static boolean validaCaractersSenha(String senha, int opcao) {
 		int qtdCaracterEncontrado = 0;
@@ -75,8 +72,11 @@ public class Questao02 {
 				if ("!@#$%^&*()-+".contains(Character.toString(c))) {
 					qtdCaracterEncontrado++;
 				}
-			}
+				
+			} 
+
 		}
+
 		if (qtdCaracterEncontrado > 0) {
 			return true;
 		} else {
@@ -85,5 +85,3 @@ public class Questao02 {
 	}
 
 }
-
-
---
+++ academiacapgemini/src/academiacapgemini/questoes/Questao03.java
@@ -0,0 +1,39 @@
+package academiacapgemini.questoes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Questao03 {
+
+	public static void main(String[] args) {
+	    
+		// Entrada de dados
+		System.out.println("Digite uma palavra: ");
+		Scanner sc = new Scanner(System.in);
+	    String palavra = sc.next();
+	   
+	    // Array para armazenas anagramas
+	    ArrayList<String> anagramas = new ArrayList<String>();
+	    
+	    // logica do anagrama
+	    for(int i = 0; i <palavra.length(); i++) {
+	        for(int j = 1; j < palavra.length(); j++) {
+	            if(palavra.charAt(i) == palavra.charAt(j) && i !=j && i < j) {
+	                if(palavra.charAt(i) == palavra.charAt(i+1)) {
+	                String x = palavra.substring(i, i + 1);
+	                anagramas.add(x);
+	                } else {
+	                String y = palavra.substring(i, j);
+	                String z = palavra.substring(i + 1, j + 1);
+	                String h = palavra.substring(i, i + 1);
+	                anagramas.add(h);
+	                anagramas.add(y);
+	                anagramas.add(z);
+	                }
+	            }
+	        }
+	    }
+	    
+	    System.out.println("A palavra contém: " + anagramas.size() + " Anagramas");
+	}
+}
